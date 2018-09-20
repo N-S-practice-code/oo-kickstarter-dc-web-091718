74a0dc317a564d98ba97adbb10b9288d33dc185c
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-20 19:07:59 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,9 @@
 class Backer
     attr_reader :name
-  attr_accessor :projects
+  attr_accessor :backed_projects
   def initialize(name)
     @name=name
+    @backed_projects=[]
   end
   
   def back_project(project)
