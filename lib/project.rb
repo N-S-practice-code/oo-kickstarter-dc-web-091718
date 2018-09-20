eca19d1f9f97cb65938fbf6bb5b9a239ab19ceb6
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-20 19:10:05 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,13 @@
 class Project
     attr_reader :title
-  attr_accessor :mood
+  attr_accessor :backers
   def initialize(title)
     @title=title
     @mood='nervous'
   end
   
-  def add_backer
+  def add_backer(backer)
+    
+  end
   
 end
